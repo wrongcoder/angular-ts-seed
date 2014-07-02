4ad6acc1a79c4dd7fe4ab7e1e168e1ffaa627979
wrongcoder <git@wrongcoder.com>
2014-07-01 21:52:35 -0700

    Add reload button in dev mode

--- app/src/RootController.ts
+++ app/src/RootController.ts
@@ -3,7 +3,25 @@
 module Application {
 
 	export class RootController {
+		private $templateCache: ng.ITemplateCacheService;
+		private $timeout: ng.ITimeoutService;
+
 		view: string = "helloWorld";
+
+		//noinspection JSUnusedGlobalSymbols
+		static $inject = [ "$templateCache", "$timeout" ];
+
+		constructor($templateCache: ng.ITemplateCacheService, $timeout: ng.ITimeoutService) {
+			this.$templateCache = $templateCache;
+			this.$timeout = $timeout;
+		}
+
+		reloadTemplates(): void {
+			var view = this.view;
+			this.$timeout(() => this.view = view, 0);
+			this.$templateCache.removeAll();
+			this.view = undefined;
+		}
 	}
 
 }
